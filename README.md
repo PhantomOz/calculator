f24407714730e0beeab6f8a64acd789247fbc500
PhantomOz <faniogor@gmail.com>
2021-01-31 01:14:15 +0100

    calculate

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ _The calculator is run through the CLI & can do the following:_
 * Addition
 * Subtracton
 * Using Pi - _Using the math library_
+* Carry out log in  base10 
+* Perform sin, cos, & tan
+
+_The Calculator is very easy to use_
 
 _The Logo was Created using ASCII ART_  
 **All you have to do is clone and run**
+
+**_Have Fun!!!_**
--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 # This is a functional calculator
-from math import pi
+from math import pi, sin, sqrt, log10, cos, tan
 from logo import logo
 print(logo)
 print("""
-Welcome to Calcuflex.
+
+
+WELCOME TO CALCUFLEX.
+
 Here are some Guidelines and functions:
 Functions -
 multiply = *
@@ -11,10 +14,22 @@ subtract = -
 Add = +
 divide = /
 pi = pi 
+sn = sin
+rt = sqrt
+lg = log10
+cs = cos
+tn = tan
 """)
+# Global Variable
+sn = sin
+rt = sqrt
+lg = log10
+cs = cos
+tn = tan
 user_input = input("Your INPUT Here_\n")
 
 def calc(inputs):
+    """This is to calculate everything from the user_input"""
     ans = eval(inputs)
     print(ans)
 
